10dc4a6899f748220e320282edb10c845328ce53
Matibej <matiasbejarano97@gmail.com>
2023-02-24 22:12:25 -0300

    carro terminado

--- assets/cart.js
+++ assets/cart.js
@@ -1,5 +1,7 @@
 const CartContainer = document.querySelector(".cart-container")
-
+const ProductsToBuy = document.querySelector(".product-list")
+const Total = document.querySelector(".total-number")
+const DeleteAll = document.querySelector(".delete-all")
 
 const saveLocalStorage = (cartList) => {
 	localStorage.setItem("cart", JSON.stringify(cartList));
@@ -10,6 +12,7 @@ let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 const RenderCartItems = (cart) => {
     let CartItem = cart.map((product) => {
+        
         return `
         
         <div class="cart-item">
@@ -29,9 +32,11 @@ const RenderCartItems = (cart) => {
 							<div class="quantity-info">
 								<span class="price-info">$${product.price}</span>
 								<div class="quantity-handler">
-									<span class="add-num">-</span>
+									<button class="reduce-num"
+                                    data-id=${product.id}>-</button>
 									<span class="number">${product.count}</span>
-									<span class="reduce-num">+</span>
+									<button class="add-num"
+                                    data-id=${product.id}>+</button>
 								</div>
 							</div>
 						</div>
@@ -52,26 +57,91 @@ const DeleteCartItems = (e) => {
     if (!e.target.classList.contains("delete")) {
         return
     }
-    console.log(e.target.dataset);
     cart = cart.filter((product) => {
         return product.id !== e.target.dataset.id;
         
     })
     saveLocalStorage(cart)
     RenderCartItems(cart)
+    RenderTotalProducts(cart)
 }
 
+const QuanitityHandler = (e) => {
+    const AddNumber = e.target.classList.contains("add-num")
+    const ReduceNumber = e.target.classList.contains("reduce-num")
+    if (AddNumber || ReduceNumber) {
+        for (let i = 0; i < cart.length; i++) {
+            if (cart[i].id == e.target.dataset.id) {
+                if ( cart[i].count < 10 && AddNumber ) {
+                    cart[i].count += 1  
+                    
+                }
+                else if (cart[i].count > 1 && ReduceNumber ) {
+                    cart[i].count -= 1
+                    
+                }
+            }
+            
+            saveLocalStorage(cart)
+            RenderCartItems(cart)
+            RenderTotalProducts(cart)
+            
+        }
+    }
+    
+}
+
+const RenderTotalProducts = (cart) => {
+    let ProductItem = cart.map((product)=>{
+        let ProductPrice = product.price*product.count
+        return `
+        <li class="buy-item">
+								<h4>${product.name}</h4>
+								<div class="items-price">
+									<p>Cantidad: ${product.count}</p>
+									<span>$${ProductPrice}</span>
+								</div>
+							</li>
+       
+        `
+        
+    })
+    ProductsToBuy.innerHTML = ProductItem.join('')
+    UpdatetotalPrice(cart)
+}
+
+
+
+const UpdatetotalPrice = (cart) => {
+    let arr1 = []
+    let total1 = 0
+    cart.forEach((product) => {
+        ProductPrice = product.price*product.count
+        arr1.push(ProductPrice)
+    });
+    const total = arr1.reduce((acc,cur) => acc + cur, total1 )
+    Total.innerHTML = `$${total}`
+}
+
+const DeleteAllProducts = (e) => {
+    if (!e.target.classList.contains("delete-all")) {
+        return
+    }
+    localStorage.clear("cart")
+    CartContainer.innerHTML = ''
+    ProductsToBuy.innerHTML = ''
+    
+}
 
 const init = () => { 
 
 RenderCartItems(cart);
-CartContainer.addEventListener("click", DeleteCartItems
-
-
-
-)
-
-
+CartContainer.addEventListener("click", (e) => {
+    QuanitityHandler(e),
+    DeleteCartItems(e)
+});
+RenderTotalProducts(cart);
+DeleteAll.addEventListener("click", DeleteAllProducts)
 
 } 
 
